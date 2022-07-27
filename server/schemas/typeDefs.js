c979f3578a9098c63e423463b8b329b79faf9be4
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-26 22:43:30 -0700

    working on app.js that will not import routes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,49 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User{
+        _id: ID!
+        username: String
+        email: String
+        password: String
+        bookCount: String
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input saveBookContent {
+        authors: [String]
+        description: String
+        title: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+
+    type Query {
+        users: [User]
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(newBook: saveBookContent!): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
