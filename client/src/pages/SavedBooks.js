fa89552cb6320551dc9b7a748d47fd0306b6e118
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-27 20:50:40 -0700

    deploying

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import {useQuery,useMutation} from '@apollo/client'
 import { GET_ME } from '../utils/queries';
